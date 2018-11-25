92d62f0747df33ea0731bf40b6df29b6b0ab27b5
Unknown <aiya88@hanyang.ac.kr>
2018-11-25 20:48:35 +0900

    ttttest

---
+++ ProjectRythm/src/test/ttttest.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package test;
+
+/**
+ * @author choeseonghwan
+ *
+ */
+public class ttttest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("print");
+
+	}
+
+}

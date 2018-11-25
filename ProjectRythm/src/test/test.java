9712e48d7e66a0ea42db4b5e8975f314b82aeb23
Unknown <aiya88@hanyang.ac.kr>
2018-11-25 20:20:46 +0900

    초기

---
+++ ProjectRythm/src/test/test.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package test;
+
+/**
+ * @author choeseonghwan
+ *
+ */
+public class test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Good");
+	}
+
+}
